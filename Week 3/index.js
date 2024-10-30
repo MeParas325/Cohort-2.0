d32f85fd9fc508dff2adf0038822dfb517d7280c
Gaurav Verma <24f2004146@ds.study.iitm.gehu.ac.in>
2024-10-30 14:17:08 +0530

    Added: Middleware in week 3

--- Week 3/index.js
+++ Week 3/index.js
@@ -2,46 +2,22 @@ const express = require('express')
 
 const app = express()
 
-function userMiddleware(req, res, next) {
-    const {username, password} = req.headers
-    if(username !== "Paras" && password !== "pass") {
-        res.status(400).json({
-            "msg": "Username or password is incorrect"
-        })
-    } else {
-        next()
-    }
-
-}
-
-function kidneyMiddleware(req, res, next) {
-
-    const kidneyId = req.query.kidneyId
-
-    if(kidneyId != 1 && kidneyId != 2) {
-        res.status(400).json({
-            "msg": "Something went wrong"
-        })
-    } else {
-        next()
-    }
-
-}
-
 app.use(express.json())
 
-app.get("/health-checkup", userMiddleware, kidneyMiddleware, (req, res) => {
+app.get("/health-checkup", (req, res) => {
 
-    const kidneyId = req.query.kidneyId
+    const kidneys = req.body.kidneys
+    const kidneyLength = kidneys.length
 
-    const username = req.headers.username
-    const password = req.headers.password
-
-    res.json({
-        msg: "Your kidney is fine"
-    })
-
+    res.send(`You have ${kidneyLength} kidneys`)
     
+})
+
+// global catches
+app.use(function(err, req, res, next) {
+    res.json({
+        'msg': "Something went wrong with our server"
+    })
 })
 
 app.listen(3000)
